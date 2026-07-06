062d7418fc1b778a4388ec314a420c36a3a6cdbf
quentinnippert <quentinnippert@gmail.com>
2026-07-06 16:57:15 +0200

    fix biome error

--- packages/client/src/events.ts
+++ packages/client/src/events.ts
@@ -1,5 +1,6 @@
 // The shared event model, fluent builder, React-free transport, and telemetry helpers.
-export * from "./events-model"
+
 export * from "./events-builder"
+export * from "./events-model"
 export * from "./events-transport"
 export * from "./telemetry-events"
